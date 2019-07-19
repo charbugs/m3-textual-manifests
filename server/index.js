d128121cdb93968195ce00dc7b1929433733e807
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-07-19 12:05:20 +0200

    --

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+//const manifest = require('../assets/iiif/text-1/manifest.json')
+
 const path = require('path')
 const express = require('express')
 var cors = require('cors')
 
 const app = express()
-const port = 8081
+
+// on heroku use process.env.PORT
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
 
 app.use(cors(), express.static(path.join(__dirname, '../public')));
 app.listen(port, () => console.log(`server listens on port ${port}!`))
