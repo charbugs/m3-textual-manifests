bf87457e3276d04ff4f645d1ce5603280d0be6aa
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-07-19 12:50:44 +0200

    --

--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ const path = require('path')
 const express = require('express')
 var cors = require('cors')
 
+
+// when running on heroku we use env variables for setup
+const port = process.env.PORT || 8080
+const home = process.env.APP_HOME || 'http://localhost:8080'
+
 const app = express()
 
 console.log(process.env)
 
-// on heroku use process.env.PORT
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8080;
-}
+
 
 app.use(cors(), express.static(path.join(__dirname, '../public')));
 app.listen(port, () => console.log(`server listens on port ${port}!`))
+
+app.listen()
